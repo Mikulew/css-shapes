fc2de05b5b7d6f6f5d9fbb6a04b5cd22435bd8d2
Mikulew <mikulew@gmail.com>
2021-07-15 08:11:10 +0200

    Configuration of VueTailwind

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueTailwind from 'vue-tailwind'
+import TButton from 'vue-tailwind/dist/t-button'
 
 Vue.config.productionTip = false
+
+const settings = {
+  't-button': {
+    component: TButton,
+  },
+}
+
+Vue.use(VueTailwind, settings)
 
 new Vue({
   render: h => h(App),
